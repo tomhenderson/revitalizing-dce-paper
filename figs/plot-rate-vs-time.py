706c2bc88ae6e47c04b4a0e9f12f7154f1b41a9d
Tom Henderson <tomh@tomh.org>
2022-02-18 07:03:30 -0800

    Update rate-vs-time figure and data

--- figs/plot-rate-vs-time.py
+++ figs/plot-rate-vs-time.py
@@ -28,7 +28,7 @@ for line in f2:
         clock2.append (float(columns[4]))
 f2.close()
 
-f3 = open("rate-vs-time.ns3.dat")
+f3 = open("rate-vs-time.dce11.ns3.dat")
 rate3 = []
 clock3 = []
 for line in f3:
@@ -48,16 +48,26 @@ for line in f4:
         clock4.append (float(columns[4]))
 f4.close()
 
+f5 = open("rate-vs-time.dce11.elf.user.dat")
+rate5 = []
+clock5 = []
+for line in f5:
+    if not line.startswith('#'):
+        columns = line.split()
+        rate5.append(float(columns[2]))
+        clock5.append (float(columns[4]))
+f5.close()
 
 plt.xlabel('Sending rate (Mbps)')
 plt.ylabel('Wall clock time (s)')
 #plt.xlim([20,20.5])
 #plt.title(title, fontdict = {'fontsize' : 12})
 plt.axhline(y=100, color='black', linestyle='--')
-plt.plot(rate4, clock4, marker='x', color='red', label='4 hops DCE-1.11')
-plt.plot(rate, clock, marker='+', color='black', label='4 hops DCE-1.11 ELF')
-plt.plot(rate2, clock2, marker='*', color='green', label='4 hops Tazaki')
-plt.plot(rate3, clock3, marker='o', color='blue', label='4 hops ns-3')
+plt.plot(rate4, clock4, marker='x', color='red', label='DCE-1.11')
+plt.plot(rate, clock, marker='+', color='black', label='DCE-1.11 ELF')
+plt.plot(rate5, clock5, marker='v', color='orange', label='DCE-1.11 ELF user')
+plt.plot(rate2, clock2, marker='*', color='green', label='Tazaki [1]')
+plt.plot(rate3, clock3, marker='o', color='blue', label='ns-3')
 plt.legend(loc='upper left')
 plt.ticklabel_format(useOffset=False)
 #plt.show()
